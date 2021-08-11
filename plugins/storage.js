7c67ded1fadd018e614fa8a7307013dd11fa8159
Salem Ouerdani <tunecino@gmail.com>
2021-08-11 09:25:20 +0100

    working mini app

--- plugins/storage.js
+++ plugins/storage.js
@@ -1,14 +1,15 @@
-<template>
-  <div>
-    <button @click="run()">run</button>
-    <pre>{{ output }}</pre>
-  </div>
-</template>
+/* eslint-disable camelcase */
+/* eslint-disable no-unreachable */
+/* eslint-disable no-unused-vars */
 
-<script>
-import { Capacitor } from '@capacitor/core'
 import { Filesystem, Directory } from '@capacitor/filesystem'
+import write_blob from 'capacitor-blob-writer'
 import axios from 'axios'
+
+const base64ToBlob = async (base64, contentType = 'audio/mpeg') => {
+  const base64Response = await fetch(`data:${contentType};base64,${base64}`)
+  return await base64Response.blob()
+}
 
 const blobToBase64 = (blob) => {
   const reader = new FileReader()
@@ -20,12 +21,9 @@ const blobToBase64 = (blob) => {
   })
 }
 
-export default {
-  data: () => ({
-    output: [],
-  }),
-  methods: {
-    async run() {
+export default ({ app, store }, inject) => {
+  inject('storage', {
+    async start() {
       /**
        * 1. Download an audio file from internet.
        */
@@ -33,7 +31,7 @@ export default {
       const url =
         'https://traffic.omny.fm/d/clips/75853ef7-6019-417d-a769-ac7900677d7c/e93dfa05-4cda-42ea-975f-ac8800022ab8/6ebf1707-02a6-4ffc-aa81-ac88000ec7c5/audio.mp3'
 
-      this.log('downloading...')
+      console.log('downloading...')
 
       const response = await axios.get(url, { responseType: 'arraybuffer' })
 
@@ -41,48 +39,41 @@ export default {
       const contentType = response.headers['content-type'] || 'audio/mpeg'
       const blob = new Blob([buffer], { type: contentType })
 
-      this.log('file loaded', { buffer, blob, contentType })
+      console.log('file loaded', { response, buffer, blob, contentType })
 
       /**
        * 2. Save it to disk.
        */
 
-      this.log('saving...')
+      console.log('saving...')
 
       const path = 'downloads/audio.mp3'
 
-      const saved = await Filesystem.writeFile({
+      const uri = await write_blob({
         path,
         directory: Directory.Documents,
-        data: await blobToBase64(blob),
+        blob,
         recursive: true,
+        on_fallback(error) {
+          console.log({ wb__error: error })
+        },
       })
 
-      this.log('saved', saved)
+      console.log('file saved to disk.', { uri })
 
       /**
-       * 3. play saved file on disk
+       * 3. load file from disk
        */
 
-      const { uri } = await Filesystem.getUri({
+      console.log('loading...')
+
+      const { data } = await Filesystem.readFile({
         path,
         directory: Directory.Documents,
+        // encoding: Encoding.UTF8,
       })
 
-      const src = Capacitor.convertFileSrc(uri)
-
-      this.log('file to play', { uri, src })
-
-      const audio = new Audio(src)
-      audio.play()
+      console.log('file loaded', { data })
     },
-    log(message, input) {
-      console.log(message, input)
-      this.output.push(message)
-      if (input) {
-        this.output.push(input)
-      }
-    },
-  },
+  })
 }
-</script>
